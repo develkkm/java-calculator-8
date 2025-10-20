b4bc85c9160132d3c72113355335e42631c43846
develkkm <kkm8905508@daum.net>
2025-10-20 20:17:50 +0900

    refactor(AppConfig): CalculatorService 의존성 제거에 맞게 구성 단순화
    - 서비스 생성 방식을 기본 생성자로 단순화

--- src/main/java/calculator/config/AppConfig.java
+++ src/main/java/calculator/config/AppConfig.java
@@ -1,15 +1,13 @@
 package calculator.config;
 
 import calculator.controller.CalculatorController;
-import calculator.domain.Delimiters;
-import calculator.domain.Operands;
 import calculator.service.CalculatorService;
 import calculator.view.ConsoleView;
 
 public class AppConfig {
     public CalculatorController calculatorController() {
         ConsoleView view = new ConsoleView();
-        CalculatorService service = new CalculatorService(new Delimiters(), new Operands());
+        CalculatorService service = new CalculatorService();
         return new CalculatorController(service, view);
     }
 }
