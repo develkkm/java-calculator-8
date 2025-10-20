1a459ada7971dbf9c1cfc7343f3c876c05914798
develkkm <kkm8905508@daum.net>
2025-10-20 20:17:07 +0900

    test(CalculatorServiceTest): 의존성 제거에 맞춰 테스트 구조 단순화
    - 서비스 객체를 기본 생성자로 생성하도록 변경

--- src/test/java/calculator/service/CalculatorServiceTest.java
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -2,15 +2,13 @@ package calculator.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import calculator.domain.Delimiters;
-import calculator.domain.Operands;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorServiceTest {
 
-    private final CalculatorService service = new CalculatorService(new Delimiters(), new Operands());
+    private final CalculatorService service = new CalculatorService();
 
     @Test
     @DisplayName("입력이 비었다면 0을 반환한다")
