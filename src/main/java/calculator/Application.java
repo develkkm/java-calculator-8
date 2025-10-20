c037b806e2b1c83352b7111bd85c3bb7a64a63b1
develkkm <kkm8905508@daum.net>
2025-10-20 16:06:40 +0900

    feat(Application): 계산기 프로그램 실행 흐름 구성
    - main 메서드에서 각 구성 요소(ConsoleView, Delimiters, Operands, CalculatorService, CalculatorController) 생성
    - 의존성 주입을 통해 객체 간 관계 설정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.Delimiters;
+import calculator.domain.Operands;
+import calculator.service.CalculatorService;
+import calculator.view.ConsoleView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleView consoleView = new ConsoleView();
+        Delimiters delimiters = new Delimiters();
+        Operands operands = new Operands();
+        CalculatorService calculatorService = new CalculatorService(delimiters, operands);
+
+        CalculatorController calculatorController = new CalculatorController(calculatorService, consoleView);
+        calculatorController.run();
     }
 }
