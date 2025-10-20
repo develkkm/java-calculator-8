9969d914c7780af82e43cb6792c1afbf318d9e38
develkkm <kkm8905508@daum.net>
2025-10-20 16:09:13 +0900

    refactor(Application): AppConfig를 활용해 main 로직 단순화
    - Application에서 직접 객체 생성하던 로직 제거
    - AppConfig의 calculatorController()를 통해 Controller 주입받아 실행
    - main 메서드가 프로그램 실행 진입점만 담당하도록 단순화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,12 @@
 package calculator;
 
+import calculator.config.AppConfig;
 import calculator.controller.CalculatorController;
-import calculator.domain.Delimiters;
-import calculator.domain.Operands;
-import calculator.service.CalculatorService;
-import calculator.view.ConsoleView;
 
 public class Application {
     public static void main(String[] args) {
-        ConsoleView consoleView = new ConsoleView();
-        Delimiters delimiters = new Delimiters();
-        Operands operands = new Operands();
-        CalculatorService calculatorService = new CalculatorService(delimiters, operands);
-
-        CalculatorController calculatorController = new CalculatorController(calculatorService, consoleView);
+        AppConfig appConfig = new AppConfig();
+        CalculatorController calculatorController = appConfig.calculatorController();
         calculatorController.run();
     }
 }
